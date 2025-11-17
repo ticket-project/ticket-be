68cffeae226bd593a48f8a6183ab08d1dd7ec376
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-11-18 08:45:21 +0900

    feat: 회원가입 시 중복 이메일은 허용하지 않는다.

--- src/main/java/com/ticket/member/service/MemberService.java
+++ src/main/java/com/ticket/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.ticket.member.service;
 
+import com.ticket.exception.DuplicateEmailException;
 import com.ticket.member.Member;
 import com.ticket.member.dto.MemberCreateRequest;
 import com.ticket.member.dto.MemberResponse;
@@ -7,15 +8,21 @@ import com.ticket.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public MemberResponse register(final MemberCreateRequest.MemberCreateCommand memberCreateCommand) {
+        if (memberRepository.existsByEmailAddress(memberCreateCommand.getEmail())) {
+            throw new DuplicateEmailException("중복 이메일은 허용되지 않습니다.");
+        }
         final Member member = Member.register(memberCreateCommand.getEmail(), passwordEncoder.encode(memberCreateCommand.getPassword()), memberCreateCommand.getName());
         memberRepository.save(member);
         return MemberResponse.of(member);
--- src/test/java/com/ticket/member/MemberServiceTest.java
+++ src/test/java/com/ticket/member/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.ticket.member;
 
+import com.ticket.exception.DuplicateEmailException;
 import com.ticket.member.dto.MemberCreateRequest;
 import com.ticket.member.dto.MemberResponse;
 import com.ticket.member.repository.MemberRepository;
@@ -13,10 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SuppressWarnings("NonAsciiCharacters")
 @ExtendWith(MockitoExtension.class)
@@ -49,5 +50,18 @@ public class MemberServiceTest {
         );
         assertThat(response.getEmail()).isEqualTo(memberCreateCommand.getEmail());
         assertThat(response.getName()).isEqualTo(memberCreateCommand.getName());
+    }
+
+    @Test
+    void 이미_존재하는_이메일이면_회원가입에_실패한다() {
+        //given
+        MemberCreateRequest.MemberCreateCommand memberCreateCommand = new MemberCreateRequest.MemberCreateCommand("test@test.com", "1234", "ANONYMOUS");
+        when(memberRepository.existsByEmailAddress("test@test.com")).thenReturn(true);
+        //then
+        assertThatThrownBy(() -> memberService.register(memberCreateCommand)).isInstanceOf(DuplicateEmailException.class);
+        verify(memberRepository, times(1)).existsByEmailAddress("test@test.com");
+        verify(memberRepository, never()).save(any(Member.class));
+        verify(passwordEncoder, never()).encode(anyString());
+
     }
 }
