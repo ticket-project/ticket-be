c5ca00d1eb775efbb7b2522a13e587e6565252a5
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-02 08:13:52 +0900

    fix(seat): SeatStatus -> PerformanceSeatStatus로 변경

--- core/core-api/src/main/java/com/ticket/core/domain/seat/Seat.java
+++ core/core-api/src/main/java/com/ticket/core/domain/seat/Seat.java
@@ -1,12 +1,8 @@
 package com.ticket.core.domain.seat;
 
-import com.ticket.storage.db.core.SeatStatus;
-
 public class Seat {
 
     private final Long id;
-
-    private Long performanceId;
 
     //행
     private final String x;
@@ -14,14 +10,10 @@ public class Seat {
     //열
     private final String y;
 
-    private SeatStatus status;
-
-    public Seat(final Long id, final Long performanceId, final String x, final String y, final SeatStatus status) {
+    public Seat(final Long id, final String x, final String y) {
         this.id = id;
-        this.performanceId = performanceId;
         this.x = x;
         this.y = y;
-        this.status = status;
     }
 
     public Long getId() {
@@ -34,13 +26,5 @@ public class Seat {
 
     public String getY() {
         return y;
-    }
-
-    public Long getPerformanceId() {
-        return performanceId;
-    }
-
-    public SeatStatus getStatus() {
-        return status;
     }
 }
