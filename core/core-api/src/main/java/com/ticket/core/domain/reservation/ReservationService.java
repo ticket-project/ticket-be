363f253ab868a011b8c66d0aa9f5d3cd801a9162
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-02 08:59:11 +0900

    feat(reservation): 예매 기능 구현

--- core/core-api/src/main/java/com/ticket/core/domain/reservation/ReservationService.java
+++ core/core-api/src/main/java/com/ticket/core/domain/reservation/ReservationService.java
@@ -2,19 +2,48 @@ package com.ticket.core.domain.reservation;
 
 import com.ticket.core.domain.member.Member;
 import com.ticket.core.domain.member.MemberFinder;
+import com.ticket.core.domain.performance.Performance;
+import com.ticket.core.domain.performance.PerformanceFinder;
+import com.ticket.core.enums.PerformanceSeatStatus;
+import com.ticket.storage.db.core.*;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReservationService {
 
     private final MemberFinder memberFinder;
+    private final PerformanceFinder performanceFinder;
+    private final PerformanceSeatRepository performanceSeatRepository;
+    private final ReservationRepository reservationRepository;
+    private final ReservationDetailRepository reservationDetailRepository;
 
-    public ReservationService(final MemberFinder memberFinder) {
+    public ReservationService(final MemberFinder memberFinder,
+                              final PerformanceFinder performanceFinder,
+                              final PerformanceSeatRepository performanceSeatRepository,
+                              final ReservationRepository reservationRepository,
+                              final ReservationDetailRepository reservationDetailRepository
+    ) {
         this.memberFinder = memberFinder;
+        this.performanceFinder = performanceFinder;
+        this.performanceSeatRepository = performanceSeatRepository;
+        this.reservationRepository = reservationRepository;
+        this.reservationDetailRepository = reservationDetailRepository;
     }
 
-    public void reserve(final Member member, final NewReservation newReservation) {
-        final Member foundMember = memberFinder.find(member.getId());
+    public void reserve(final NewReservation newReservation) {
+        final Member foundMember = memberFinder.find(newReservation.getMemberId());
+        final Performance foundPerformance = performanceFinder.find(newReservation.getPerformanceId());
+        final List<PerformanceSeatEntity> performanceSeats = performanceSeatRepository.findByPerformanceIdAndSeatIdInAndStatus(
+                newReservation.getPerformanceId(), newReservation.getSeatIds(), PerformanceSeatStatus.AVAILABLE
+        );
 
+        final ReservationEntity savedReservation = reservationRepository.save(new ReservationEntity(foundMember.getId(), foundPerformance.getId()));
+        reservationDetailRepository.saveAll(
+                performanceSeats.stream()
+                .map(p -> new ReservationDetailEntity(savedReservation.getId(), p.getId()))
+                .toList()
+        );
     }
 }
--- storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatRepository.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatRepository.java
@@ -1,6 +1,11 @@
 package com.ticket.storage.db.core;
 
+import com.ticket.core.enums.PerformanceSeatStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface PerformanceSeatRepository extends JpaRepository<PerformanceSeatEntity, Long> {
+    List<PerformanceSeatEntity> findByPerformanceIdAndSeatIdInAndStatus(Long performanceId, Collection<Long> seatIds, PerformanceSeatStatus status);
 }
