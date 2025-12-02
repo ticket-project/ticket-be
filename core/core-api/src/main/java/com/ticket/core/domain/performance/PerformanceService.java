1284f4ad11a718882082055426297646da452cd1
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-02 09:24:33 +0900

    fix(performance): 불필요한 코드 제거

--- core/core-api/src/main/java/com/ticket/core/domain/performance/PerformanceService.java
+++ core/core-api/src/main/java/com/ticket/core/domain/performance/PerformanceService.java
@@ -15,21 +15,4 @@ public class PerformanceService {
         this.seatRepository = seatRepository;
     }
 
-    //TODO PerformanceSeat으로 이동 필요
-//    public List<Seat> findAllSeatByPerformance(final Long performanceId) {
-//        final Long availableCount = seatRepository.countByPerformanceIdAndStatus(performanceId, SeatStatus.AVAILABLE);
-//        if (availableCount <= 0) throw new CoreException(ErrorType.NOT_EXIST_AVAILABLE_SEAT);
-//        final List<SeatEntity> seatEntities = seatRepository.findByPerformanceId(performanceId);
-//        if (seatEntities.isEmpty()) {
-//            throw new NotFoundException(ErrorType.NOT_FOUND);
-//        }
-//        return seatEntities.stream()
-//                .map(e -> new Seat(e.getId(),
-//                        e.getPerformanceId(),
-//                        e.getX(),
-//                        e.getY(),
-//                        e.getStatus())
-//                )
-//                .toList();
-//    }
 }
