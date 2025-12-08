f7013f773d7f0df8931667005d5a65cf017a26ef
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-08 09:33:05 +0900

    feat(member): Member 관련 파일에 role 추가

--- core/core-api/src/main/java/com/ticket/core/domain/member/MemberDetails.java
+++ core/core-api/src/main/java/com/ticket/core/domain/member/MemberDetails.java
@@ -1,13 +1,21 @@
 package com.ticket.core.domain.member;
 
+import com.ticket.core.enums.Role;
+
 public class MemberDetails {
     private final Long memberId;
+    private final Role role;
 
-    public MemberDetails(Long memberId) {
+    public MemberDetails(final Long memberId, final Role role) {
         this.memberId = memberId;
+        this.role = role;
     }
 
     public Long getMemberId() {
         return memberId;
+    }
+
+    public Role getRole() {
+        return role;
     }
 }
