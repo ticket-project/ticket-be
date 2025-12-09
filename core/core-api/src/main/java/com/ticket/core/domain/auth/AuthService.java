55b8cf9b506d665393092ba71277a6a0b0727c27
mn040820 <mn040820@naver.com>
2025-12-09 22:17:41 +0900

    feat(auth): PasswordService 도입

--- core/core-api/src/main/java/com/ticket/core/domain/auth/AuthService.java
+++ core/core-api/src/main/java/com/ticket/core/domain/auth/AuthService.java
@@ -7,7 +7,6 @@ import com.ticket.core.support.exception.CoreException;
 import com.ticket.core.support.exception.ErrorType;
 import com.ticket.storage.db.core.MemberEntity;
 import com.ticket.storage.db.core.MemberRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,26 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final PasswordPolicyValidator passwordPolicyValidator;
+    private final PasswordService passwordService;
 
-    public AuthService(final MemberRepository memberRepository, final PasswordEncoder passwordEncoder, final PasswordPolicyValidator passwordPolicyValidator) {
+    public AuthService(final MemberRepository memberRepository, final PasswordService passwordService) {
         this.memberRepository = memberRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.passwordPolicyValidator = passwordPolicyValidator;
+        this.passwordService = passwordService;
     }
 
     @Transactional
     public Long register(final AddMember addMember) {
-        //비밀번호 정책 관련 많이 나올듯 해서 분리함.
-        passwordPolicyValidator.validateAdd(addMember.getPassword());
-        //이메일 정책은 중복 밖에 없을듯 해서 굳이 분리하지 않음.
         if (memberRepository.existsByEmail(addMember.getEmail())) {
             throw new CoreException(ErrorType.DUPLICATE_EMAIL_ERROR);
         }
         final MemberEntity savedMember = memberRepository.save(new MemberEntity(
                 addMember.getEmail(),
-                passwordEncoder.encode(addMember.getPassword()),
+                passwordService.encode(addMember.getPassword()),
                 addMember.getName(),
                 addMember.getRole()
         ));
@@ -45,7 +39,7 @@ public class AuthService {
         final MemberEntity foundMemberEntity = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND));
 
-        if (!passwordEncoder.matches(password, foundMemberEntity.getPassword())) {
+        if (!passwordService.matches(password, foundMemberEntity.getPassword())) {
             throw new CoreException(ErrorType.NOT_MATCH_PASSWORD);
         }
         return new Member(foundMemberEntity.getId(), new Email(foundMemberEntity.getEmail()), foundMemberEntity.getName(), foundMemberEntity.getRole());
---
+++ core/core-api/src/main/java/com/ticket/core/domain/auth/PasswordService.java
@@ -0,0 +1,22 @@
+package com.ticket.core.domain.auth;
+
+import com.ticket.core.domain.member.vo.Password;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordService {
+    private final PasswordEncoder passwordEncoder;
+
+    public PasswordService(final PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String encode(final Password password) {
+        return passwordEncoder.encode(password.getValue());
+    }
+
+    public boolean matches(final String requestPassword, final String realPassword) {
+        return passwordEncoder.matches(requestPassword, realPassword);
+    }
+}
