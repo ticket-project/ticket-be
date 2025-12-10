4045a938369693d6fe722b9ebb14eb7df04df954
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-10 10:09:02 +0900

    fix(member): controller request에서 검증 로직 bean validation으로 대체

--- core/core-api/src/main/java/com/ticket/core/api/controller/v1/request/AddMemberRequest.java
+++ core/core-api/src/main/java/com/ticket/core/api/controller/v1/request/AddMemberRequest.java
@@ -3,12 +3,15 @@ package com.ticket.core.api.controller.v1.request;
 import com.ticket.core.domain.member.AddMember;
 import com.ticket.core.enums.Role;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class AddMemberRequest {
 
     @Email
     private String email;
+    @NotBlank
     private String password;
+    @NotBlank
     private String name;
 
     public AddMemberRequest() {}
@@ -26,15 +29,6 @@ public class AddMemberRequest {
     }
 
     public AddMember toAddMember() {
-        if (password.isEmpty()) {
-            throw new IllegalArgumentException("비밀번호는 공백일 수 없습니다.");
-        }
-        if (email.isEmpty()) {
-            throw new IllegalArgumentException("이메일은 공백일 수 없습니다.");
-        }
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
-        }
         return new AddMember(email, password, name, Role.MEMBER);
     }
 }
