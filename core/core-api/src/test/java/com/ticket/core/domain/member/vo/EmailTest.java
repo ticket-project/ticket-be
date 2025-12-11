07fab0a26a23eb89561a632703890e0daab8784c
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-11 09:20:22 +0900

    test(email): 올바른 이메일이면 Email 생성에 성공한다

--- core/core-api/src/test/java/com/ticket/core/domain/member/vo/EmailTest.java
+++ core/core-api/src/test/java/com/ticket/core/domain/member/vo/EmailTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -16,5 +17,11 @@ class EmailTest {
     void 올바르지_않은_이메일이면_Email_생성에_실패한다(final String email) {
         //then
         assertThatThrownBy(() -> Email.create(email)).isInstanceOf(CoreException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"user@test.com", "user.name+tag@sub.domain.co"})
+    void 올바른_이메일이면_Email_생성에_성공한다(final String email) {
+        assertThatCode(() -> Email.create(email)).doesNotThrowAnyException();
     }
 }
