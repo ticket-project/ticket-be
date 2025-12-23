730983c6dc4ce392f7a8e2319063dc993d856523
HuSung <128116509+LeeHusung@users.noreply.github.com>
2025-12-24 08:13:45 +0900

    feat(reservation): 무한 대기 방지를 위한 락 타임아웃 설정

--- storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatRepository.java
+++ storage/db-core/src/main/java/com/ticket/storage/db/core/PerformanceSeatRepository.java
@@ -2,12 +2,17 @@ package com.ticket.storage.db.core;
 
 import com.ticket.core.enums.PerformanceSeatState;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 
 import java.util.List;
 
 public interface PerformanceSeatRepository extends JpaRepository<PerformanceSeatEntity, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+            @QueryHint(name = "javax.persistence.lock.timeout", value = "3000")
+    })
     List<PerformanceSeatEntity> findByPerformanceIdAndSeatIdInAndState(Long performanceId, List<Long> seatIds, PerformanceSeatState state);
 }
