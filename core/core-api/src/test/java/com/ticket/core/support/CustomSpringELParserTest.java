5eec33240af45c0084a47ec668cf7f07c154bfc6
mn040820 <mn040820@naver.com>
2026-01-04 11:25:15 +0900

    test(CustomSpringELParserTest): 여러 SpEL 표현식이면 각각 평가된 결과에 prefix가 붙어 리스트로 반환된다(순서 보장)

--- core/core-api/src/test/java/com/ticket/core/support/CustomSpringELParserTest.java
+++ core/core-api/src/test/java/com/ticket/core/support/CustomSpringELParserTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings("NonAsciiCharacters")
 class CustomSpringELParserTest {
 
     @Test
@@ -19,5 +20,19 @@ class CustomSpringELParserTest {
         final List<String> keys = CustomSpringELParser.getDynamicValue(prefix, parameterNames, args, dynamicKey);
         //then
         assertThat(keys).containsExactly("LOCK:100");
+    }
+
+    @Test
+    void 여러_SPEL_표현식이면_각_평가된_결과에_prefix가_붙어_리스트로_반환된다_순서보장() {
+        //given
+        String prefix = "LOCK:";
+        String[] parameterNames = {"seatId", "memberId"};
+        Object[] args = {100L, 1L};
+        String[] dynamicKey = {"#seatId", "#memberId"};
+        //when
+        final List<String> keys = CustomSpringELParser.getDynamicValue(prefix, parameterNames, args, dynamicKey);
+        //then
+        assertThat(keys).containsExactly("LOCK:100", "LOCK:1");
+
     }
 }
