73afe7ebb7532a3f63ca7863e1d36183583678bb
mn040820 <mn040820@naver.com>
2026-01-04 12:10:16 +0900

    test(concurrent): 동시성 테스트 util 객체 생성

--- core/core-api/src/test/java/com/ticket/core/domain/hold/SeatHoldConcurrencyServiceV0IntegrationTest.java
+++ core/core-api/src/test/java/com/ticket/core/domain/hold/SeatHoldConcurrencyServiceV0IntegrationTest.java
@@ -8,6 +8,7 @@ import com.ticket.core.domain.performanceseat.PerformanceSeatRepository;
 import com.ticket.core.enums.Role;
 import com.ticket.core.support.IntegrationBase;
 import com.ticket.core.support.TestDataFactory;
+import com.ticket.util.ConcurrentTestUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,16 +16,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+@SuppressWarnings("NonAsciiCharacters")
 class SeatHoldConcurrencyServiceV0IntegrationTest extends IntegrationBase {
 
     private static final Logger log = LoggerFactory.getLogger(SeatHoldConcurrencyServiceV0IntegrationTest.class);
@@ -57,42 +52,12 @@ class SeatHoldConcurrencyServiceV0IntegrationTest extends IntegrationBase {
 
     @Test
     void 재고가_1개일때_여러_요청이_동시에_들어오면_비관적_락에_의해_한_명만_선점한다() throws InterruptedException {
-        // given
-        final int threadCount = 100;
-        try (final ExecutorService es = Executors.newVirtualThreadPerTaskExecutor()) {
-        CountDownLatch ready = new CountDownLatch(threadCount);
-        CountDownLatch start = new CountDownLatch(1);
-        CountDownLatch done = new CountDownLatch(threadCount);
-        AtomicInteger successCount = new AtomicInteger(0);
-        AtomicInteger failCount = new AtomicInteger(0);
-
-        for (int i = 0; i < threadCount; i++) {
-            int idx = i;
-            es.submit(() -> {
-                try {
-                    ready.countDown();
-                    start.await();
-                    final NewSeatHold request = new NewSeatHold(
-                            savedMembers.get(idx).getId(),
-                            savedPerformance.getId(),
-                            List.of(1L)
-                    );
-                    holdService.hold(request);
-                    successCount.incrementAndGet();
-                } catch (Exception e) {
-                    log.error("선점 실패: {}", e.getMessage(), e);
-                    failCount.incrementAndGet();
-                } finally {
-                    done.countDown();
-                }
-            });
-        }
-        ready.await();
-        start.countDown();
-        done.await();
-        assertThat(successCount.get()).isEqualTo(1);
-        assertThat(failCount.get()).isEqualTo(99);
-        }
+        // given & when & then
+        ConcurrentTestUtil.execute(100, () -> holdService.hold(new NewSeatHold(
+                savedMembers.getFirst().getId(),
+                savedPerformance.getId(),
+                List.of(1L)
+        )));
     }
 
 }
---
+++ core/core-api/src/test/java/com/ticket/util/ConcurrentTestUtil.java
@@ -0,0 +1,46 @@
+package com.ticket.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConcurrentTestUtil {
+    private static final Logger log = LoggerFactory.getLogger(ConcurrentTestUtil.class);
+
+    public static void execute(final int threadCount, Runnable runnable) throws InterruptedException {
+        try (final ExecutorService es = Executors.newVirtualThreadPerTaskExecutor()) {
+            CountDownLatch ready = new CountDownLatch(threadCount);
+            CountDownLatch start = new CountDownLatch(1);
+            CountDownLatch done = new CountDownLatch(threadCount);
+            AtomicInteger successCount = new AtomicInteger(0);
+            AtomicInteger failCount = new AtomicInteger(0);
+
+            for (int i = 0; i < threadCount; i++) {
+                es.submit(() -> {
+                    try {
+                        ready.countDown();
+                        start.await();
+                        runnable.run();
+                        successCount.incrementAndGet();
+                    } catch (Exception e) {
+                        log.error("선점 실패: {}", e.getMessage(), e);
+                        failCount.incrementAndGet();
+                    } finally {
+                        done.countDown();
+                    }
+                });
+            }
+            ready.await();
+            start.countDown();
+            done.await();
+            assertThat(successCount.get()).isEqualTo(1);
+            assertThat(failCount.get()).isEqualTo(99);
+        }
+    }
+}
