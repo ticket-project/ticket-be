f2f324055e2d98944b3335d35c1e907968916f73
mn040820 <mn040820@naver.com>
2026-01-24 23:46:53 +0900

    feat(show): 조회수 컬럼 추가

--- core/core-api/src/main/java/com/ticket/core/domain/show/Show.java
+++ core/core-api/src/main/java/com/ticket/core/domain/show/Show.java
@@ -23,18 +23,21 @@ public class Show extends BaseEntity {
 
     private LocalDate endDate;
 
+    private Long viewCount;
+
     //장소 todo 향후 물리적 공연장 테이블 나와서 연관관계?
     private String place;
 
     protected Show() {}
 
-    public Show(final String title, final String subTitle, final String info, final LocalDate startDate, final LocalDate endDate, final String place) {
+    public Show(final String title, final String subTitle, final String info, final LocalDate startDate, final LocalDate endDate, final String place, final Long viewCount) {
         this.title = title;
         this.subTitle = subTitle;
         this.info = info;
         this.startDate = startDate;
         this.endDate = endDate;
         this.place = place;
+        this.viewCount = viewCount;
     }
 
     public Long getId() {
@@ -63,5 +66,9 @@ public class Show extends BaseEntity {
 
     public String getPlace() {
         return place;
+    }
+
+    public Long getViewCount() {
+        return viewCount;
     }
 }
