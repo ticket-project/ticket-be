d48f37e0aa4746fd1111126bc786ded56f15de34
HuSung <128116509+LeeHusung@users.noreply.github.com>
2026-02-04 08:24:44 +0900

    fix(auth): cors 전체 허용(추후 허용된 origin만으로 변경)

--- core/core-api/src/main/java/com/ticket/core/config/WebConfig.java
+++ core/core-api/src/main/java/com/ticket/core/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ticket.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -21,4 +22,14 @@ public class WebConfig implements WebMvcConfigurer {
 //                .addPathPatterns("/api/**")
 //                .excludePathPatterns("/api/v1/auth/**");
 //    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
