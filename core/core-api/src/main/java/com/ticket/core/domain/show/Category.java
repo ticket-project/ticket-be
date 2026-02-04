0ac1dae2d13c67209b96478f4dca700679318058
HuSung <128116509+LeeHusung@users.noreply.github.com>
2026-02-05 08:57:00 +0900

    feat(category): baseentity 추가

--- core/core-api/src/main/java/com/ticket/core/domain/show/Category.java
+++ core/core-api/src/main/java/com/ticket/core/domain/show/Category.java
@@ -1,10 +1,11 @@
 package com.ticket.core.domain.show;
 
+import com.ticket.core.domain.BaseEntity;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "CATEGORYS")
-public class Category {
+public class Category extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
