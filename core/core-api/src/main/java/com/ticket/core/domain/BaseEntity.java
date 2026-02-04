b449c312d358a2c63a16a7ec6721a60be6b73079
HuSung <128116509+LeeHusung@users.noreply.github.com>
2026-02-05 08:49:01 +0900

    feat(base): 생성자, 수정자 컬럼 추가

--- core/core-api/src/main/java/com/ticket/core/domain/BaseEntity.java
+++ core/core-api/src/main/java/com/ticket/core/domain/BaseEntity.java
@@ -2,7 +2,9 @@ package com.ticket.core.domain;
 
 import com.ticket.core.enums.EntityStatus;
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -13,11 +15,18 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false, nullable = false)
+    protected String createdBy;
+
+    @LastModifiedBy
+    protected String updatedBy;
 
     @Enumerated(EnumType.STRING)
     private EntityStatus status = EntityStatus.ACTIVE;
@@ -28,6 +37,14 @@ public class BaseEntity {
 
     public LocalDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
     }
 
     public EntityStatus getStatus() {
