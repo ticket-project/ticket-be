79d9085bfb8d56a22a71e4e95dc130a3f966878b
HuSung <128116509+LeeHusung@users.noreply.github.com>
2026-02-05 09:43:51 +0900

    feat(auditor): jpaauditor 설정

--- core/core-api/src/main/java/com/ticket/core/config/JpaAuditingConfig.java
+++ core/core-api/src/main/java/com/ticket/core/config/JpaAuditingConfig.java
@@ -1,9 +1,16 @@
 package com.ticket.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
 }
