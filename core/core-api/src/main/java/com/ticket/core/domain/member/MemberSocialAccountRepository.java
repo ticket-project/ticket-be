c75e29d20153866aafb0ab13621e7709cc283cdd
mn040820 <mn040820@naver.com>
2026-02-20 18:39:31 +0900

    fix(social-login): jpa 지연 로딩 문제로 인한 소셜 재로그인 에러 수정

--- core/core-api/src/main/java/com/ticket/core/domain/member/MemberSocialAccountRepository.java
+++ core/core-api/src/main/java/com/ticket/core/domain/member/MemberSocialAccountRepository.java
@@ -2,12 +2,15 @@ package com.ticket.core.domain.member;
 
 import com.ticket.core.enums.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberSocialAccountRepository extends JpaRepository<MemberSocialAccount, Long> {
 
-    Optional<MemberSocialAccount> findBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId);
+    @Query("SELECT msa FROM MemberSocialAccount msa JOIN FETCH msa.member WHERE msa.socialProvider = :socialProvider AND msa.socialId = :socialId")
+    Optional<MemberSocialAccount> findBySocialProviderAndSocialId(@Param("socialProvider") SocialProvider socialProvider, @Param("socialId") String socialId);
 
     Optional<MemberSocialAccount> findByMemberAndSocialProvider(Member member, SocialProvider socialProvider);
 }
