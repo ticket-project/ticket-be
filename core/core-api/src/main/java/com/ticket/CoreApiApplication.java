6ebeb1dbc4f76884406762fa88d167d0637d0240
mn040820 <mn040820@naver.com>
2026-03-08 13:24:42 +0900

    chore: 미사용 hold 클래스 제거 및 설정 정리

--- core/core-api/src/main/java/com/ticket/CoreApiApplication.java
+++ core/core-api/src/main/java/com/ticket/CoreApiApplication.java
@@ -2,10 +2,8 @@ package com.ticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class CoreApiApplication {
 
     public static void main(String[] args) {
