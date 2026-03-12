5c77d7fa7e9dd9dcd5504ac30c2936bec499cbd1
HuSung <128116509+LeeHusung@users.noreply.github.com>
2026-03-12 13:38:15 +0900

    refactor(base): 불필요한 조회 API와 getter를 정리

--- core/core-api/src/main/java/com/ticket/core/domain/performanceseat/repository/PerformanceSeatRepository.java
+++ core/core-api/src/main/java/com/ticket/core/domain/performanceseat/repository/PerformanceSeatRepository.java
@@ -1,36 +1,16 @@
 package com.ticket.core.domain.performanceseat.repository;
 
-import com.ticket.core.domain.performance.Performance;
 import com.ticket.core.domain.performanceseat.model.PerformanceSeat;
-import com.ticket.core.domain.seat.Seat;
 import com.ticket.core.enums.PerformanceSeatState;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.*;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface PerformanceSeatRepository extends JpaRepository<PerformanceSeat, Long> {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints({
-            @QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")
-    })
-    List<PerformanceSeat> findByPerformanceIdAndSeatIdInAndState(Long performanceId, List<Long> seatIds, PerformanceSeatState state);
-
-    @Modifying
-    @Query("""
-            UPDATE PerformanceSeat ps
-            SET ps.state = :changeState
-            WHERE ps.state = :curState
-            AND ps.performance = :performance
-            AND ps.seat in :seats
-    """)
-    int updateState(Performance performance, List<Seat> seats, PerformanceSeatState curState, PerformanceSeatState changeState);
 
     List<PerformanceSeat> findAllByPerformanceIdAndSeatIdIn(Long performanceId, Collection<Long> seatIds);
 
     List<PerformanceSeat> findAllByStateEquals(PerformanceSeatState state);
 
-    List<PerformanceSeat> findAllByPerformanceAndSeatIn(Performance performance, Collection<Seat> seats);
 }
