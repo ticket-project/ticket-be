1a4b477019c2f96a0bc443e807ed9662ac2b30ac
HuSung <128116509+LeeHusung@users.noreply.github.com>
2026-03-20 17:01:52 +0900

    refactor(core): queue 재진입과 좌석 해제 분기 정리

--- core/core-api/src/main/java/com/ticket/core/domain/performanceseat/command/SeatSelectionService.java
+++ core/core-api/src/main/java/com/ticket/core/domain/performanceseat/command/SeatSelectionService.java
@@ -20,48 +20,28 @@ public class SeatSelectionService {
     private final SeatSelectionStore seatSelectionStore;
 
     public void select(final Long performanceId, final Long seatId, final Long memberId) {
-        final boolean locked = seatSelectionStore.selectIfAbsent(performanceId, seatId, memberId.toString(), SELECT_TTL);
+        final String memberKey = memberKeyOf(memberId);
+        final boolean locked = seatSelectionStore.selectIfAbsent(performanceId, seatId, memberKey, SELECT_TTL);
         if (!locked) {
             log.warn("좌석 선택에 실패했습니다. performanceId={}, seatId={}, memberId={}", performanceId, seatId, memberId);
             throw new CoreException(ErrorType.SEAT_ALREADY_SELECTED);
         }
-
         log.info("좌석 선택에 성공했습니다. performanceId={}, seatId={}, memberId={}", performanceId, seatId, memberId);
     }
 
     public void deselect(final Long performanceId, final Long seatId, final Long memberId) {
-        final String memberKey = memberId.toString();
-
+        final String memberKey = memberKeyOf(memberId);
         final String holder = seatSelectionStore.getHolder(performanceId, seatId);
         if (holder == null) {
             log.info("좌석 선택 해제를 건너뜁니다. 이미 선택 정보가 없습니다. performanceId={}, seatId={}", performanceId, seatId);
             return;
         }
-
-        if (!memberKey.equals(holder)) {
-            log.warn("좌석 선택 해제 권한이 없습니다. performanceId={}, seatId={}, requestMemberId={}, holderMemberId={}",
-                    performanceId, seatId, memberId, holder);
-            throw new CoreException(ErrorType.SEAT_NOT_OWNED);
-        }
-
-        final boolean released = seatSelectionStore.releaseIfOwned(performanceId, seatId, memberKey);
-        if (!released) {
-            final String currentHolder = seatSelectionStore.getHolder(performanceId, seatId);
-            if (currentHolder == null) {
-                log.info("좌석 선택 해제 시점에 이미 만료되었거나 해제되었습니다. performanceId={}, seatId={}, memberId={}",
-                        performanceId, seatId, memberId);
-                return;
-            }
-
-            log.warn("좌석 선택 해제 권한이 없습니다. performanceId={}, seatId={}, requestMemberId={}, holderMemberId={}",
-                    performanceId, seatId, memberId, currentHolder);
-            throw new CoreException(ErrorType.SEAT_NOT_OWNED);
-        }
-        log.info("좌석 선택 해제에 성공했습니다. performanceId={}, seatId={}, memberId={}", performanceId, seatId, memberId);
+        validateOwner(performanceId, seatId, memberId, memberKey, holder);
+        releaseSeat(performanceId, seatId, memberId, memberKey);
     }
 
     public DeselectedSeatIds deselectAll(final Long performanceId, final Long memberId) {
-        final String memberKey = memberId.toString();
+        final String memberKey = memberKeyOf(memberId);
         final List<Long> deselectedSeatIds = seatSelectionStore.releaseAllByMember(performanceId, memberKey);
         logDeselectedSeats(performanceId, memberId, deselectedSeatIds);
         return DeselectedSeatIds.from(deselectedSeatIds);
@@ -73,6 +53,54 @@ public class SeatSelectionService {
 
     public java.util.Set<Long> getSelectingSeatIds(final Long performanceId) {
         return seatSelectionStore.getSelectingSeatIds(performanceId);
+    }
+
+    private String memberKeyOf(final Long memberId) {
+        return memberId.toString();
+    }
+
+    private void validateOwner(
+            final Long performanceId,
+            final Long seatId,
+            final Long memberId,
+            final String memberKey,
+            final String holder
+    ) {
+        if (memberKey.equals(holder)) {
+            return;
+        }
+        logNotOwned(performanceId, seatId, memberId, holder);
+        throw new CoreException(ErrorType.SEAT_NOT_OWNED);
+    }
+
+    private void releaseSeat(
+            final Long performanceId,
+            final Long seatId,
+            final Long memberId,
+            final String memberKey
+    ) {
+        final boolean released = seatSelectionStore.releaseIfOwned(performanceId, seatId, memberKey);
+        if (released) {
+            log.info("좌석 선택 해제에 성공했습니다. performanceId={}, seatId={}, memberId={}", performanceId, seatId, memberId);
+            return;
+        }
+        handleReleaseFailure(performanceId, seatId, memberId);
+    }
+
+    private void handleReleaseFailure(final Long performanceId, final Long seatId, final Long memberId) {
+        final String currentHolder = seatSelectionStore.getHolder(performanceId, seatId);
+        if (currentHolder == null) {
+            log.info("좌석 선택 해제 시점에 이미 만료되었거나 해제되었습니다. performanceId={}, seatId={}, memberId={}",
+                    performanceId, seatId, memberId);
+            return;
+        }
+        logNotOwned(performanceId, seatId, memberId, currentHolder);
+        throw new CoreException(ErrorType.SEAT_NOT_OWNED);
+    }
+
+    private void logNotOwned(final Long performanceId, final Long seatId, final Long memberId, final String holder) {
+        log.warn("좌석 선택 해제 권한이 없습니다. performanceId={}, seatId={}, requestMemberId={}, holderMemberId={}",
+                performanceId, seatId, memberId, holder);
     }
 
     private void logDeselectedSeats(final Long performanceId, final Long memberId, final List<Long> seatIds) {
